eaa6dd68c6826e8ca056ef550c0332721e0f54a0
KHACHANI <ahmedkhachani@gmail.com>
2019-03-04 17:01:19 +0100

    finished presentation Service

--- src/app/components/presentationComponent/presentation/presentation.component.ts
+++ src/app/components/presentationComponent/presentation/presentation.component.ts
@@ -9,6 +9,8 @@ import { ChairModule } from 'src/app/modules/chair/chair.module';
 import { ChairService } from 'src/app/services/chair.service';
 import { PresentationModule } from 'src/app/modules/presentation/presentation.module';
 import { RoleModule } from 'src/app/modules/role/role.module';
+import { Router } from '@angular/router';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-presentation',
@@ -18,49 +20,50 @@ import { RoleModule } from 'src/app/modules/role/role.module';
 export class PresentationComponent implements OnInit {
   article = new ArticleModule(0, '', '', new DomaineModule(0, ''));
   chair = new ChairModule(0, "", "", new RoleModule(0, ""));
-  articles: Array<ArticleModule>;
-  allArticles: Array<ArticleModule>;
+  articlesAccepted: Array<ArticleModule>;
+  allArticlesAccepted: Array<ArticleModule>;
   chairs: Array<ChairModule>;
   domaines: Array<DomaineModule>;
   private conferences: Array<any>
-  private conference: Conference = new Conference();
-  presentation = new PresentationModule(0, this.conference, this.article, this.chair);
+  private conference = new Conference();
+  presentation = new PresentationModule(0, new Conference(), new ArticleModule(0, '', '', new DomaineModule(0, '')), this.chair);
 
 
-  constructor(private presentationService: PresentationService, private conferenceService: ConferenceService, private articleService: ArticleService, private chairService: ChairService) { }
+  constructor(private presentationService: PresentationService, private conferenceService: ConferenceService,
+    private articleService: ArticleService, private chairService: ChairService,
+    private router: Router) { }
 
   ngOnInit() {
     this.getAllArticlesAccepted();
-    this.getAllConferences();
-    this.getAllChairs()
+
+  }
+  navigateTo(path) {
+    this.router.navigate([path]);
   }
   getAllArticlesAccepted() {
     this.articleService.getAllAccepted()
       .subscribe(data => {
-        this.allArticles = data,
-          // this.articles = data,
-          console.log(data)
+        this.allArticlesAccepted = data,
+          this.articlesAccepted = data
       }, error => console.log(error));
   }
   getAllChairs() {
     this.chairService.getAll()
       .subscribe(data => {
         this.chairs = data,
-          // this.articles = data,
-          console.log(data)
+        console.log(data)
       }, error => console.log(error));
   }
 
   getAllConferences() {
     this.conferenceService.getAll()
       .subscribe(data => {
-        this.conferences = data,
-          console.log(data)
+        this.conferences = data
       }, error => console.log(error));
   }
 
   search(title) {
-    this.articles = this.allArticles.filter(item => (this.filterByTitle(item, title) || this.filterByDomaine(item, title)));
+    this.articlesAccepted = this.allArticlesAccepted.filter(item => (this.filterByTitle(item, title) || this.filterByDomaine(item, title)));
   }
 
   filterByTitle(item, title) {
@@ -68,6 +71,32 @@ export class PresentationComponent implements OnInit {
   }
   filterByDomaine(item, domaineName) {
     return item.domaine.name.toLowerCase().includes(domaineName.toLowerCase());
+  }
+  // show(article) {
+  //   this.navigateTo('articles/show/' + article.id);
+  // }
+
+  ObjetSelected(objet) {
+    if (objet != null && this.articlesAccepted != null) {
+      this.article = objet
+      this.presentation.article = this.article
+      this.articlesAccepted = null;
+      this.getAllConferences();
+    } else if (this.article != null && this.conferences != null) {
+      this.conference = objet;
+      this.presentation.conference = objet;
+      this.conferences=null;
+      this.getAllChairs()
+    } else if (this.article != null && this.conference != null && this.chairs != null) {
+      this.chair=objet
+      this.presentation.chair=this.chair
+      this.chairs = null
+      this.getAllArticlesAccepted();
+      this.presentationService.create(this.presentation).subscribe(data => {
+        console.log(data)
+      }, error => console.log(error));
+    }
+
   }
 
 
