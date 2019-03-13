7cec46fff2310fb7e5fd58a1805cfc4ef89487cc
agadhicham <hicham95ag@gmail.com>
2019-03-13 11:30:06 +0100

    crud 1er chair

--- src/app/components/select-option-to-administration/select-option-to-administration.component.ts
+++ src/app/components/select-option-to-administration/select-option-to-administration.component.ts
@@ -1,3 +1,6 @@
+import { Router } from '@angular/router';
+import { ChairService } from './../../services/chair.service';
+import { ChairModule } from './../../modules/chair/chair.module';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./select-option-to-administration.component.css']
 })
 export class SelectOptionToAdministrationComponent implements OnInit {
-
-  constructor() { }
+   chaire:ChairModule;
+  constructor(private chair:ChairService ,private router:Router) { }
 
   ngOnInit() {
+  }
+
+  saveChair(dataForm) {
+    console.log(dataForm)
+    this.chair.create(dataForm)
+      .subscribe(data => {
+        console.log(data)
+        console.log("chair added succefuly")
+        this.router.navigate(['home'])
+
+      }, error => {
+        console.log(error)
+      })
+
+
   }
 
 }
