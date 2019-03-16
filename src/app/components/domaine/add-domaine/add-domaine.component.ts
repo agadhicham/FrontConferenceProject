32ffae5a4affed1c20c289a987bfc1586c867328
agadhicham <hicham95ag@gmail.com>
2019-03-16 01:42:39 +0100

    domaine

--- src/app/components/domaine/add-domaine/add-domaine.component.ts
+++ src/app/components/domaine/add-domaine/add-domaine.component.ts
@@ -1,3 +1,6 @@
+import { Router } from '@angular/router';
+import { DomaineService } from 'src/app/services/domaine.service';
+import { DomaineModule } from 'src/app/modules/domaine/domaine.module';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-domaine.component.css']
 })
 export class AddDomaineComponent implements OnInit {
+  chaire: DomaineModule=new DomaineModule(0,"");
 
-  constructor() { }
+  constructor(private chair:DomaineService, private router:Router) { }
 
   ngOnInit() {
+
   }
+
+  saveChair(dataForm) {
+    console.log(dataForm)
+    this.chair.create(dataForm)
+      .subscribe(data => {
+        console.log(data)
+        console.log("doamine added succefuly")
+        this.router.navigate(['domaineList'])
+
+      }, error => {
+        console.log(error)
+      })
+
+
+  }
+
 
 }
--- src/app/components/domaine/list-domaine/list-domaine.component.ts
+++ src/app/components/domaine/list-domaine/list-domaine.component.ts
@@ -1,4 +1,8 @@
+import { Router } from '@angular/router';
+import { DomaineService } from './../../../services/domaine.service';
+import { DomaineModule } from './../../../modules/domaine/domaine.module';
 import { Component, OnInit } from '@angular/core';
+import { Route } from '@angular/router';
 
 @Component({
   selector: 'app-list-domaine',
@@ -6,10 +10,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-domaine.component.css']
 })
 export class ListDomaineComponent implements OnInit {
-
-  constructor() { }
+  private idChair:number;
+  chaire:DomaineModule= new DomaineModule(0,"")
+   private chaires: Array<any>
+  constructor(private chairService: DomaineService, private router:Router) { }
 
   ngOnInit() {
+    this.chairService.getAll().subscribe(data=>{
+
+      this.chaires=data
+    },error=>{
+      console.log(error)
+    })
+
+
+  }
+
+  deletChair(id:number)
+  {
+    this.chairService.remove(id)
+    .subscribe(data=>{
+      console.log('deleting.............. of this conference it done')
+      alert('chaire deleted with success')
+      this.router.navigate(['chairList'])
+    },error=>{
+      console.log(error)
+    })
+  }
+
+  editChair(chair)
+  {
+
+    this.chaire=chair;
+  }
+
+  updateChair()
+  {
+    this.chairService.edit(this.chaire)
+    .subscribe(data=>{
+      console.log('update of this chair it done')
+      alert('alert updating with success')
+    },error=>{
+      console.log(error)
+    })
   }
 
 }
